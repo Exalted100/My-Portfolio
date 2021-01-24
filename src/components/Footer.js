daabf4abac95d12f09ff5d977ec7f3c447d67628
Fawaz Haroun <harounayomikun@gmail.com>
2021-01-24 13:23:13 +0100

    fixed a design bug

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div style={{textAlign: "center", paddingBottom: "1em", fontStyle: "italic", paddingTop: "3em", backgroundColor: "inherit"}}>
+        <div style={{textAlign: "center", paddingBottom: "1em", fontStyle: "italic", paddingTop: "3em", backgroundColor: "inherit", marginTop: 0}}>
             <p>
                 Copyright 2021, Fawaz Haroun
             </p>
