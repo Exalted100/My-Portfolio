eef294ac06c2238c0d7ae4e20413a3067641515f
Fawaz Haroun <harounayomikun@gmail.com>
2021-04-20 06:30:54 +0100

    fixed a typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
                     <p className="stack">HTML, CSS, JS, React</p>
                     <p className="type">Web</p>
                     <p className="role">Front End Developer</p>
-                    <p className="info">Hello, and welcome! I am a front end web developer. I like recreating aesthetically pleasing and easy to use designs. I am currently learning backend web development with Nodejs, Express, and MongoDB. When I'm not watching more episodes of anime than I can count in a single sitting, I will likely to reading through Medium posts.</p>
+                    <p className="info">Hello, and welcome! I am a front end web developer. I like recreating aesthetically pleasing and easy to use designs. I am currently learning backend web development with Nodejs, Express, and MongoDB. When I'm not watching more episodes of anime than I can count in a single sitting, I will likely be reading through Medium posts.</p>
                 </div>
             </div>
             <p className="explore">EXPLORE!</p>
